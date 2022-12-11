85ac7172cd5e1a2644fbef63a14e2be3933db01a
pdrobnjak <drobnjakpavle95@gmail.com>
2022-12-11 16:26:37 +0100

    Fix secp256k1 cgo duplication

--- go-ethereum/crypto/signature_cgo.go
+++ go-ethereum/crypto/signature_cgo.go
@@ -25,7 +25,7 @@ import (
 	"fmt"
 
 	"github.com/tenderly/nitro/go-ethereum/common/math"
-	"github.com/tenderly/nitro/go-ethereum/crypto/secp256k1"
+	"github.com/ethereum/go-ethereum/crypto/secp256k1"
 )
 
 // Ecrecover returns the uncompressed public key that created the given signature.
