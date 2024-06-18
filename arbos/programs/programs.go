72704757a83f3d0692d5135f86c638a13b96c4e5
Nikola Bebić‮ <nikola.bebic@tenderly.co>
2024-06-18 13:28:30 +0200

    fix wasm initialization

--- arbos/programs/programs.go
+++ arbos/programs/programs.go
@@ -8,16 +8,16 @@ import (
 	"fmt"
 	"math/big"
 
-	"github.com/tenderly/nitro/go-ethereum/common"
-	"github.com/tenderly/nitro/go-ethereum/core"
-	"github.com/tenderly/nitro/go-ethereum/core/state"
-	"github.com/tenderly/nitro/go-ethereum/core/vm"
-	"github.com/tenderly/nitro/go-ethereum/log"
 	"github.com/tenderly/nitro/arbcompress"
 	"github.com/tenderly/nitro/arbos/addressSet"
 	"github.com/tenderly/nitro/arbos/storage"
 	"github.com/tenderly/nitro/arbos/util"
 	"github.com/tenderly/nitro/arbutil"
+	"github.com/tenderly/nitro/go-ethereum/common"
+	"github.com/tenderly/nitro/go-ethereum/core"
+	"github.com/tenderly/nitro/go-ethereum/core/state"
+	"github.com/tenderly/nitro/go-ethereum/core/vm"
+	"github.com/tenderly/nitro/go-ethereum/log"
 	am "github.com/tenderly/nitro/util/arbmath"
 )
 
@@ -153,7 +153,8 @@ func (p Programs) ActivateProgram(evm *vm.EVM, address common.Address, runMode c
 	}
 	// replace the cached asm
 	if cached {
-		cacheProgram(statedb, info.moduleHash, programData, params, debugMode, time, runMode)
+		code := statedb.GetCode(address)
+		cacheProgram(statedb, info.moduleHash, programData, code, codeHash, params, debugMode, time, runMode)
 	}
 
 	return stylusVersion, codeHash, info.moduleHash, dataFee, false, p.setProgram(codeHash, programData)
@@ -210,7 +211,7 @@ func (p Programs) CallProgram(
 	statedb.AddStylusPages(program.footprint)
 	defer statedb.SetStylusPagesOpen(open)
 
-	localAsm, err := getLocalAsm(statedb, moduleHash, contract.Address(), params.PageLimit, evm.Context.Time, debugMode, program)
+	localAsm, err := getLocalAsm(statedb, moduleHash, contract.Address(), contract.Code, contract.CodeHash, params.PageLimit, evm.Context.Time, debugMode, program)
 	if err != nil {
 		log.Crit("failed to get local wasm for activated program", "program", contract.Address())
 		return nil, err
@@ -247,6 +248,10 @@ func (p Programs) CallProgram(
 
 func getWasm(statedb vm.StateDB, program common.Address) ([]byte, error) {
 	prefixedWasm := statedb.GetCode(program)
+	return getWasmFromContractCode(prefixedWasm)
+}
+
+func getWasmFromContractCode(prefixedWasm []byte) ([]byte, error) {
 	if prefixedWasm == nil {
 		return nil, ProgramNotWasmError()
 	}
@@ -395,7 +400,12 @@ func (p Programs) SetProgramCached(
 		return err
 	}
 	if cache {
-		cacheProgram(db, moduleHash, program, params, debug, time, runMode)
+		// Not passing in an address is supported pre-Verkle, as in Blockchain's ContractCodeWithPrefix method.
+		code, err := db.Database().ContractCode(common.Address{}, codeHash)
+		if err != nil {
+			return err
+		}
+		cacheProgram(db, moduleHash, program, code, codeHash, params, debug, time, runMode)
 	} else {
 		evictProgram(db, moduleHash, program.version, debug, runMode, expired)
 	}
