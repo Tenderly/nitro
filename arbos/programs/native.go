9c1341f77259173c7aeb27e370006c9d5d1ec323
pdrobnjak <drobnjakpavle95@gmail.com>
2024-07-23 16:17:39 +0200

    Fix cgo directives

--- arbos/programs/native.go
+++ arbos/programs/native.go
@@ -8,7 +8,8 @@ package programs
 
 /*
 #cgo CFLAGS: -g -Wall -I../../target/include/
-#cgo LDFLAGS: ${SRCDIR}/../../target/lib/libstylus.a -ldl -lm
+#cgo arm64 LDFLAGS: ${SRCDIR}/../../target/lib/arm64/libstylus.a -ldl -lm
+#cgo amd64 LDFLAGS: ${SRCDIR}/../../target/lib/amd64/libstylus.a -ldl -lm
 #include "arbitrator.h"
 
 typedef uint8_t u8;
--- execution/gethexec/executionengine.go
+++ execution/gethexec/executionengine.go
@@ -8,7 +8,8 @@ package gethexec
 
 /*
 #cgo CFLAGS: -g -Wall -I../../target/include/
-#cgo LDFLAGS: ${SRCDIR}/../../target/lib/libstylus.a -ldl -lm
+#cgo arm64 LDFLAGS: ${SRCDIR}/../../target/lib/arm64/libstylus.a -ldl -lm
+#cgo amd64 LDFLAGS: ${SRCDIR}/../../target/lib/amd64/libstylus.a -ldl -lm
 #include "arbitrator.h"
 */
 import "C"
